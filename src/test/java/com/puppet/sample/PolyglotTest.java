f441a1d5fa96512820fe8e9c434a4b5382eb5890
golisanthu <157676217+golisanthu@users.noreply.github.com>
2024-03-11 14:13:41 +0530

    Update PolyglotTest.java

--- src/test/java/com/puppet/sample/PolyglotTest.java
+++ src/test/java/com/puppet/sample/PolyglotTest.java
@@ -7,5 +7,26 @@ public class PolyglotTest
 {
     @Test
     public void testEnMsg() {
-        assertEquals("Well Come To My Tomcat Server!", new Polyglot().enMsg());
+        assertEquals("Hello World!", new Polyglot().enMsg());
+    }
+
+    @Test
+    public void testZhMsg() {
+        assertEquals("你好，世界!", new Polyglot().zhMsg());
+    }
+
+    @Test
+    public void testSpMsg() {
+        assertEquals("¡Hola Mundo!", new Polyglot().spMsg());
+    }
+
+    @Test
+    public void testHiMsg() {
+        assertEquals("नमस्ते दुनिया!", new Polyglot().hiMsg());
+    }
+
+    @Test
+    public void testArMsg() {
+        assertEquals("مرحبا بالعالم!", new Polyglot().arMsg());
+    }
 }
